d0c48a0b94d86be0f5fbfb49b78e22e275a531cb
Lauren Glynn <lauren.glynn@gmail.com>
2022-08-09 19:50:07 -0700

    User input

--- calculator.py
+++ calculator.py
@@ -1,7 +1,15 @@
 """CLI application for a prefix-notation calculator."""
 
+from xml.etree.ElementTree import TreeBuilder
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
 # Replace this with your code
+def tokenize_input():
+    user_input = input('> ')
+    tokens = user_input.split(" ")
+
+    while True:
+        if tokens == "+":
+            
